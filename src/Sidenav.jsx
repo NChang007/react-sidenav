7deaabebdaa41236705719f3060f8f02beff6db4
Nelson Chang <nelsonc924@gmail.com>
2023-09-16 20:44:24 -0400

    initial setup

---
+++ src/Sidenav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sidenav = () => {
+  return (
+    <nav>
+        <div className='title'>
+
+        </div>
+        <div className="navigation">
+
+        </div>
+        <div className="user-content"></div>
+    </nav>
+  )
+}
+
+export default Sidenav
